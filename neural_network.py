ee88bf18e0dcfb30fb34482a43ae39318ac2c1e7
sajidmahmud69 <sajidmahmud136@yahoo.com>
2021-01-04 02:19:22 -0500

    second commit

--- neural_network.py
+++ neural_network.py
@@ -4,16 +4,56 @@ import numpy as np
 
 # actually make the input batches
 
-inputs = [[1, 2, 3, 2,5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]   # Shape is 3x4
+# inputs = [[1, 2, 3, 2.5], 
+#           [2.0, 5.0, -1.0, 2.0], 
+#           [-1.5, 2.7, 3.3, -0.8]]        # Shape is (3x4)
+
  
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]   # Shape is 3x4
+# weights = [[0.2, 0.8, -0.5, 1.0],
+#            [0.5, -0.91, 0.26, -0.5],
+#            [-0.26, -0.27, 0.17, 0.87]]   # Shape is (3x4)
+# biases = [2, 3, 0.5]                     # Shape is (3, )
 
-biases = [2, 3, 0.5]
+# weights2 = [[0.1, -0.14, 0.5],
+#            [-0.5, 0.12, -0.33],
+#            [-0.44, 0.73, -0.13]]         # Shape is (3x4)
+# biases2 = [-1, 2, -0.5]                  # Shape is (3, )
 
+
+np.random.seed (0)
+X = [[1, 2, 3, 2.5], 
+     [2.0, 5.0, -1.0, 2.0], 
+     [-1.5, 2.7, 3.3, -0.8]]        # Shape is (3x4)
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        """
+        Constructor function for the class
+
+        Params:
+                n_inputs: size of the input layer coming in
+                n_neurons: how many neurons should this layer have
+        Outputs:
+                No outputs. Just set the weights and biases on a layer based on the params
+        """
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)                     # Shape of weights (n_inputs, n_neurons)
+        self.biases = np.zeros ((1, n_neurons))                                        # Shape of baises (1, n_neurons)
+
+    def forward (self, inputs):
+        """
+        Set the weights and biases for the next layer
+        Params:
+                inputs: the inputs of the previous layer
+        """
+        self.output = np.dot (inputs, self.weights) + self.biases
+
+layer1 = Layer_Dense (4,5)
+layer2 = Layer_Dense (5,2)                                       # layer2 first param has to match layer1 last param
+
+layer1.forward (X)
+# print (layer1.output)
+layer2.forward (layer1.output)
+# print (layer2.output)
 
 # SHAPES 
 """
@@ -61,10 +101,9 @@ which in turn has 4 elements
 
 
 # shorter way to do output_neuron = activation * weights + bias
-# outputs = np.dot (weights, inputs) + biases
-# print (outputs)
 
 
+# layer1_outputs = np.dot (inputs, np.array (weights).T) + biases
+# layer2_outputs = np.dot (layer1_outputs, np.array (weights2).T) + biases2
 
-
- 
+# print (layer2_outputs)

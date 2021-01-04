e66333e6177325924f71a812424b762220281fb2
sajidmahmud69 <sajidmahmud136@yahoo.com>
2021-01-04 03:12:11 -0500

    third commit

--- neural_network.py
+++ neural_network.py
@@ -20,11 +20,43 @@ import numpy as np
 # biases2 = [-1, 2, -0.5]                  # Shape is (3, )
 
 
-np.random.seed (0)
+"""np.random.seed (0)
 X = [[1, 2, 3, 2.5], 
      [2.0, 5.0, -1.0, 2.0], 
      [-1.5, 2.7, 3.3, -0.8]]        # Shape is (3x4)
+"""
 
+# DATASET
+def create_data (points, classes):
+    X = np.zeros ((points * classes, 2))
+    y = np.zeros (points * classes, dtype= "uint8")
+    for class_number in range (classes):
+        ix = range (points * class_number, points * (class_number + 1))
+        r = np.linspace (0.0, 1, points) # radius
+        t = np.linspace (class_number * 4, (class_number + 1) * 4, points) + np.random.randn (points) * 0.2
+        X[ix] = np.c_[r * np.sin (t * 2.5), r * np.cos (t * 2.5)]
+        y[ix] = class_number
+    return X, y
+
+import matplotlib.pyplot as plt
+
+print ("here")
+X, y = create_data (100, 3)
+
+plt.scatter (X[:, 0], X[:, 1])
+plt.show ()
+
+plt.scatter (X[:, 0], X[:, 1], c = y, cmap= 'brg')
+plt.show ()
+
+
+
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init ()
+X, y = spiral_data (100, 3)                 # 100 feature sets of 3 classes
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         """
@@ -47,12 +79,22 @@ class Layer_Dense:
         """
         self.output = np.dot (inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense (4,5)
+    
+class Activation_ReLU:
+    def forward (self, inputs):
+        self.output = np.maximum (0, inputs)
+
+layer1 = Layer_Dense (2,5)
+activation1= Activation_ReLU ()
+
 layer2 = Layer_Dense (5,2)                                       # layer2 first param has to match layer1 last param
 
 layer1.forward (X)
 # print (layer1.output)
-layer2.forward (layer1.output)
+activation1.forward (layer1.output)
+print (activation1.output)
+# print (layer1.output)
+# layer2.forward (layer1.output)
 # print (layer2.output)
 
 # SHAPES 

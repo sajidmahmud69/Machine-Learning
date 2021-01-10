9c4f14c5586b2aa88d3a2690a3a8feee42e171be
sajidmahmud69 <54687706+sajidmahmud69@users.noreply.github.com>
2021-01-10 00:01:44 -0500

    Add files via upload

---
+++ ML Model Deployment/tweets.py
@@ -0,0 +1,21 @@
+from twitter_api import api
+import time
+
+def get_related_tweets(text_query):
+    # list to store tweets
+    tweets_list = []
+    # number of tweets
+    count = 50
+    try:
+        # Pulling individual tweets from query
+        for tweet in api.search(q=text_query, count=count):
+            print(tweet.text)
+            # Adding to list that contains all tweets
+            tweets_list.append({'created_at': tweet.created_at,
+                                'tweet_id': tweet.id,
+                                'tweet_text': tweet.text})
+        return pd.DataFrame.from_dict(tweets_list)
+
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        time.sleep(3)

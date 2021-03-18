e4c25c62b37528955394162353fded2b8a1f0263
sajidmahmud69 <54687706+sajidmahmud69@users.noreply.github.com>
2021-03-17 21:36:38 -0400

    Delete ML Model Deployment directory

--- ML Model Deployment/Readme.md
+++
@@ -1,2 +0,0 @@
-### This is a simple LogisticRegression Machine Learning Model deployed using Flask python
-#### The purpose of this is to have a model that can detect hate speech in a tweet
--- ML Model Deployment/use_model.py
+++
@@ -1,9 +0,0 @@
-# Import joblib
-from joblib import load
-
-text = ["Virat Kohli, AB de Villiers set to auction their 'Green Day' kits from 2016 IPL match to raise funds"]
-
-# Load the model from a save file
-pipeline = load ("text_classification.joblib")
-
-print (pipeline.predict (text))
